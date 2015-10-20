fc20cdb3dbbc05bc5aeb3daa9b607bb2a36fe03a
Damon_Yuan <yzmhit@live.com>
2015-10-20 12:08:30 +0800

    first commit

---
+++ README.md
@@ -0,0 +1,72 @@
+# react-native-progress-dialog-android
+
+React native android module to present progress dialog.
+
+### Installation
+
+```bash
+npm install --save react-native-progress-dialog-android
+```
+
+### Add it to your android project
+
+* In `android/setting.gradle`
+
+```gradle
+...
+include ':DYProgressDialogAndroid', ':app'
+project(':DYProgressDialogAndroid').projectDir = new File(rootProject.projectDir, '../node_modules/react-native-progress-dialog-android')
+```
+
+* In `android/app/build.gradle`
+
+```gradle
+...
+dependencies {
+    ...
+    compile project(':DYProgressDialogAndroid')
+}
+```
+
+* register module (in MainActivity.java)
+
+```java
+import com.damonyuan.dyprogressdialogandroid.DYProgressDialogAndroidPackage;;  // <--- import
+
+public class MainActivity extends Activity implements DefaultHardwareBackBtnHandler {
+  ......
+
+  @Override
+  protected void onCreate(Bundle savedInstanceState) {
+    super.onCreate(savedInstanceState);
+    mReactRootView = new ReactRootView(this);
+
+    mReactInstanceManager = ReactInstanceManager.builder()
+      .setApplication(getApplication())
+      .setBundleAssetName("index.android.bundle")
+      .setJSMainModuleName("index.android")
+      .addPackage(new MainReactPackage())
+      .addPackage(new DYProgressDialogAndroidPackage())              // <------ add here
+      .setUseDeveloperSupport(BuildConfig.DEBUG)
+      .setInitialLifecycleState(LifecycleState.RESUMED)
+      .build();
+
+    mReactRootView.startReactApplication(mReactInstanceManager, "Example", null);
+
+    setContentView(mReactRootView);
+  }
+
+  ......
+
+}
+```
+
+## Example
+```javascript
+var ProgressDialog = require('react-native-progress-dialog-android');
+
+ProgressDialog.createDialog();
+```
+## License
+
+MIT
---
+++ src/java/com/DYProgressDialogAndroid/DYProgressDialogAndroidModule.java
@@ -0,0 +1,64 @@
+package com.damonyuan.dyprogressdialogandroid;
+
+import android.app.ProgressDialog;
+import android.content.Context;
+
+import com.facebook.react.bridge.NativeModule;
+import com.facebook.react.bridge.ReactApplicationContext;
+import com.facebook.react.bridge.ReactContext;
+import com.facebook.react.bridge.ReactContextBaseJavaModule;
+import com.facebook.react.bridge.ReactMethod;
+import com.facebook.react.common.MapBuilder;
+
+public class DYProgressDialogAndroidModule extends ReactContextBaseJavaModule {
+    private Context context;
+    private ProgressDialog progressDialog;
+
+    public DYProgressDialogAndroidModule(ReactApplicationContext reactContext) {
+        super(reactContext);
+        this.context = reactContext;
+    }
+
+    @Override
+    public String getName() { return "ProgressDialogAndroid"; }
+
+    @ReactMethod
+    public void createDialog() {
+        this.progressDialog = new ProgressDialog(this.context);
+    }
+
+    @ReactMethod
+    public void setCancelable(boolean cancelable) {
+        this.progressDialog.setCancelable(cancelable);
+    }
+    
+    @ReactMethod
+    public void setMessage(String message) {
+        this.progressDialog.setMessage(message);
+    }
+
+    @ReactMethod
+    public void setProgressStyle(int progressStyle) {
+        this.progressDialog.setProgressStyle(progressStyle);
+    }
+
+    @ReactMethod
+    public void setProgress(int progress) {
+        this.progressDialog.setProgress(progress);
+    }
+
+    @ReactMethod
+    public void setMax(int max) {
+        this.progressDialog.setMax(max);
+    }
+
+    @ReactMethod
+    public void show() {
+        this.progressDialog.show();
+    }
+
+    @ReactMethod
+    public void dismiss() {
+        this.progressDialog.dismiss();
+    }
+}
